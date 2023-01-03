0dc9c5664861d185a7c29b206ae5e930437d2a20
Mahesh Hegde <net.mahesh29@gmail.com>
2023-01-03 09:51:57 +0530

    use rand_r in monte carlo

--- PADP_Lab/program1/monte_carlo.c
+++ PADP_Lab/program1/monte_carlo.c
@@ -4,9 +4,11 @@
 #include <math.h>
 #include <omp.h>
 
-const int minIter = 1000;
+const int minIter = 100000;
 
 const int maxIter = minIter * 1000;
+
+int counts[4] = {0, 0, 0, 0};
 
 double compute(int nIter) {
 	double x, y, z, pi;
@@ -15,16 +17,24 @@ double compute(int nIter) {
 	// check how many points are inside 1/4th a circle (area pi/4)
 	// (pi/4)/1 = count / niter (proportionality, assuming good randomness)
 	// pi = 4 * (count / niter)
-	#pragma omp parallel for
+	#pragma omp parallel
+	{
+	int localCount = 0;
+	unsigned int randomState = 9231028;
+	#pragma omp for
 	for (int i = 0; i < nIter; i++) {
-		x = (double)rand() / RAND_MAX;
-		y = (double)rand() / RAND_MAX;
-		if ((x*x + y*y) <= 1) count++;
+		x = (double)rand_r(&randomState) / RAND_MAX;
+		y = (double)rand_r(&randomState) / RAND_MAX;
+		if ((x*x + y*y) <= 1) localCount++;
+	}
+	#pragma omp critical
+	count += localCount;
 	}
 	return 4 * (double)count/nIter;
 }
 
 int main(int argc, char **argv) {
+	srandom(clock());
 	int k = 0; // number of iterations
 	printf("%-20s%-20d%-20d%-20d%-20d\n", "iterations", 1, 2, 4, 8);
 	for (int nIter = minIter; nIter <= maxIter; nIter *= 10) {
@@ -32,10 +42,10 @@ int main(int argc, char **argv) {
 		for (int i = 1; i <= 8; i*=2) {
 			omp_set_num_threads(i);
 			double pi;
-			clock_t begin = clock();
+			double begin = omp_get_wtime();
 			pi = compute(nIter);
-			clock_t end = clock();
-			printf("%-8.4lg \e[31;1m%-6.4lg\e[0m|\t", (double)(end - begin)/CLOCKS_PER_SEC * 1000, pi);
+			double end = omp_get_wtime();
+			printf("%-8.4lg \e[31;1m%-6.4lg\e[0m|\t", (end - begin) * 1000, pi);
 		}
 		printf("\n");
 	}
