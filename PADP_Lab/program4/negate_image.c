a9064782d789cae13cbb82897ca3263d24585f13
Mahesh Hegde <net.mahesh29@gmail.com>
2023-02-11 17:13:41 +0530

    Update program 4

--- PADP_Lab/program4/negate_image.c
+++ PADP_Lab/program4/negate_image.c
@@ -1,54 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
+#include <omp.h>
 #include <gd.h>
 
-#include <omp.h>
+char dest[512] = {0};
 
 void check(int condition, const char *error) {
 	if (!condition) {
-		fprintf(stderr, "Error: %s\n", error);
+		fprintf(stderr, "Error: %s", error);
+		perror("Last reported system error is: ");
 		exit(1);
 	}
 }
-// gdImagePtr (type), gdImageCreateFromPng, gdImageS{X,Y}, gdImageGetPixel,
-// gdImage{RGB}, gdImageColorAllocate, gdImagePng, gdImageSetPixel, gdImageDestroy
 
-int main(int argc, char **argv) {
-	FILE *fp = NULL;
-	gdImagePtr img;
-	char *iname, *oname;
-	if (argc != 3) {
-		printf("usage: %s input output\n", argv[0]);
-		return 1;
-	}
-	iname = argv[1];
-	oname = argv[2];
-	int w, h, i = 0;
-	fp = fopen(iname, "r");
+void negate(const char *path, double *time, int printDims) {
+	FILE *fp = fopen(path, "r");
 	check(fp != NULL, "cannot open input file");
-	img = gdImageCreateFromPng(fp);
-	w = gdImageSX(img);
-	h = gdImageSY(img);
+	gdImagePtr img = gdImageCreateFromPng(fp);
 	double begin = omp_get_wtime();
+	int w = gdImageSX(img);
+	int h = gdImageSY(img);
 	#pragma omp parallel for
 	for (int x = 0; x < w; x++) {
 		for (int y = 0; y < h; y++) {
 			int color = gdImageGetPixel(img, x, y);
-			int avgColor = 255 - (gdImageRed(img, color) + gdImageGreen(img, color) 
-				+ gdImageBlue(img, color)) / 3;
-			int allocColor = gdImageColorAllocate(img, avgColor, avgColor, avgColor);
-			#pragma omp critical
-			gdImageSetPixel(img, x, y, allocColor);
+			int inv = 255 - ((gdImageRed(img, color) + gdImageGreen(img, color) + gdImageBlue(img, color))/3);
+			int alloc = gdImageColorAllocate(img, inv, inv, inv);
+			gdImageSetPixel(img, x, y, alloc);
 		}
 	}
 	double end = omp_get_wtime();
-	FILE *out = fopen(oname, "w");
-	check(out != NULL, "cannot open output file");
-	gdImagePng(img, out);
+	sprintf(dest, "%s.neg.png", path);
 	fclose(fp);
+	FILE *out = fopen(dest, "w");
+	gdImagePng(img, out);
 	fclose(out);
-	gdImageDestroy(img);
-	printf("%.4lg ms\n", (end - begin)/3);
-	return 0;
+	if (printDims) {
+		int pch = printf("%d x %d | ", w, h);
+		printf("%*s", 20 - pch, "");
+	}
+	*time = end - begin;
+}
+
+int main(int argc, char **argv) {
+	if (argc == 1) {
+		fprintf(stderr, "please provide arguments\n");
+	}
+	printf ("%-20s%-20s%-20s%-20s%-20s\n", "Dims", "1", "2", "4", "8");
+	for (int i = 1; i < argc; i++) {
+		for (int th = 1; th <= 8; th *= 2) {
+			omp_set_num_threads(th);
+			double time;
+			negate(argv[i], &time, th == 1);
+			printf("%-17.4lg | ", time);
+		}
+		printf("\n");
+	}
 }
