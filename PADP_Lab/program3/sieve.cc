03ef6eaee9c6134a09240c65acf8cd7e713eb59e
Mahesh Hegde <net.mahesh29@gmail.com>
2023-02-15 08:37:26 +0530

    Complete Program 3

--- PADP_Lab/program3/sieve.cc
+++ PADP_Lab/program3/sieve.cc
@@ -8,7 +8,7 @@ using namespace std;
 
 /// Strike marks all composite numbers in the range `[i, limit]`, in the intervals
 /// of `step`.
-inline int strike(vector<bool>& composite, int i, int step, int limit) {
+inline int strike(vector<int>& composite, int i, int step, int limit) {
 	for (; i <= limit; i+=step) {
 		composite[i] = 1;
 	}
@@ -16,7 +16,7 @@ inline int strike(vector<bool>& composite, int i, int step, int limit) {
 }
 
 int cacheUnfriendlySieve(int n, double *time) {
-	vector<bool> composite(n+1, 0);
+	vector<int> composite(n+1, 0);
 	int m = sqrt(n);
 	int count = 0;
 	double begin = omp_get_wtime();
@@ -35,7 +35,7 @@ int cacheUnfriendlySieve(int n, double *time) {
 }
 
 int cacheFriendlySieve(int n, double *time) {
-	vector<bool> composite(n+1, 0);
+	vector<int> composite(n+1, 0);
 	int m = sqrt(n);
 	int count = 0;
 	vector<int> striker, factor;
@@ -63,14 +63,60 @@ int cacheFriendlySieve(int n, double *time) {
 	return count;
 }
 
-int (*func[])(int, double *) = {cacheUnfriendlySieve, cacheFriendlySieve};
+int parallelSieve(int n, double *time) {
+	int count = 0, m = (int)sqrt(n);
+	vector<int> factor;
+	double begin = omp_get_wtime();
+
+	// compute initial set of points
+	vector<int> sv(m+1, false);
+	for (int i = 2; i <= m; i++) {
+		if (!sv[i]) {
+			count++;
+			factor.push_back(i);
+			strike(sv, 2*i, i, m);
+		}
+	}
+	#pragma omp parallel
+	{
+		// Assuming that OpenMP will divide The inner loop into largest
+		// possible chunks. In that case we have to compute the striker
+		// initially (if it's not computed in particular thread).
+		vector<int> sv(m+1, false);
+		vector<int> striker;
+		int base = -1;
+		#pragma omp for reduction(+:count)
+		for (int w = m+1; w <= n; w+= m) {
+			sv.assign(m+1, false); // reset
+			int limit = min(w+m-1, n) - w;
+			if (base != w) {
+				for (auto f: factor) {
+					striker.push_back((f+w-1) / f * f - w);
+				}
+			}
+			// now we have strikers
+			for (int i = 0;  i < factor.size(); i++) {
+				striker[i] = strike(sv, striker[i], factor[i], limit) - m;
+			}
+			for (int i = 0; i <= limit; i++) {
+				count += !sv[i];
+			}
+			base = limit;
+		}
+	}
+	double end = omp_get_wtime();
+	*time = end - begin;
+	return count;
+}
+
+int (*func[])(int, double *) = {cacheUnfriendlySieve, cacheFriendlySieve, parallelSieve};
 
 int main() {
 	printf("sizeof int %lu\n", sizeof(int));
-	printf("%-20s%-25s%-25s\n", "N", "cache-unfriendly", "cache-friendly");
+	printf("%-20s%-25s%-25s%-25s\n", "N", "cache-unfriendly", "cache-friendly", "parallel");
 	for (int n = 10000; n <= 10000 * 1000; n *= 10) {
 		printf("%-17d | ", n);
-		for (int i = 0; i < 2; i++) {
+		for (int i = 0; i < 3; i++) {
 			double time;
 			int count = func[i](n, &time);
 			printf("%-10.4lg (%-10d) | ", time*1000, count);
